14bdf312a80925a9200a35c030fb687668354991
mhr1226 <h.m.michiharu@gmail.com>
2024-07-16 22:37:50 +0900

    score.phpの完成

---
+++ kadai_005/score.php
@@ -0,0 +1,24 @@
+<p>
+  <?php
+  $score_1 = 80;
+  $score_2 = 60;
+  $score_3 = 55;
+  $score_4 = 40;
+  $score_5 = 100;
+  $score_6 = 25;
+  $score_7 = 80;
+  $score_8 = 95;
+  $score_9 = 30;
+  $score_10 =60;
+  // 合計点
+  $total_score = $score_1 + $score_2 + $score_3 + $score_4 + $score_5 + $score_6 + $score_7 + $score_8 + $score_9 + $score_10;
+
+  // 平均点
+  $average_score = $total_score / 10;
+
+
+  echo $total_score;
+  echo '<br>';
+  echo $average_score;
+  ?>
+</p>
