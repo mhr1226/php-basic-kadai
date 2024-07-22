3aaed2ac5c260ca62181884744bc08a3598c231f
mhr1226 <156394244+mhr1226@users.noreply.github.com>
2024-07-22 23:57:26 +0900

    add to kadai_011

---
+++ kadai_011/foreach.php
@@ -0,0 +1,13 @@
+<p>
+  <?php
+  $onion_dates = [
+    '名前' => '玉ねぎ',
+    '値段' => 200,
+    '産地' => '北海道'
+  ];
+
+  foreach($onion_dates as $key => $value){
+    echo "{$key}:{$value}<br>";
+  }
+  ?>
+</p>
